7b8f6d495238348e09f73c17275224c672d5dfe0
kastellanos <castellanos1458@hotmail.com>
2016-11-19 20:39:28 -0500

    Fixed problems

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ function validacion( claveIngresada, claveCorrecta ){
   
   }
 }
+
+
 function nivel1(event) {
 	claveIngresada = document.getElementById("clave").value;
   claveCorrecta = "MasterOfPupetsRules";
@@ -24,6 +26,13 @@ function mapa( nodes, edges, activeNode ){
                   };
                   var network = new vis.Network(container, data, {});
                 network.selectNodes(activeNode);
+                document.getElementById("clave")
+                  .addEventListener("keyup", function(event) {
+                  event.preventDefault();
+                  if (event.keyCode == 13) {
+                      document.getElementById("verifier").click();
+                  }
+                });
 }
 function mapa1(){
   // Crea un arreglo con los nodos visitados hasta ahora.
@@ -68,7 +77,7 @@ function mapa2(){
 
 function nivel3(event) {
   claveIngresada = document.getElementById("clave").value;
-  claveCorrecta = "Gaijin-desu";
+  claveCorrecta = "YoSoyElMejorHacker";
   validacion( claveIngresada,  claveCorrecta );
 }
 
@@ -79,77 +88,64 @@ function mapa3(){
       
       {id: 1, label: 'Brasil'},
       {id: 2, label: 'Finlandia'},
-      {id: 3, label: 'Japon'},
+      {id: 3, label: 'China'},
   ];
 
   // Crea un arreglo con las conexiones entre los nodos.
   var edges = [
       {from: 1, to: 2},
       {from: 2, to: 3},
+
       
+
   ];
   // Crea un arreglo con los nodos activos.
   var activeNode = [ 3 ];
   mapa( nodes, edges, activeNode );
 }
 
-function nivel4(event) {
-  claveIngresada = document.getElementById("clave").value;
-  claveCorrecta = "d3f4ult";
-  validacion( claveIngresada,  claveCorrecta );
+function caesar(){
+  textToShift = document.getElementById("caesar_input").value;
+  shift = document.getElementById("shift_input").value;
+  lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  //alert(textToShift.length);
+  var textShifted = "";
+  for (var i = 0; i < textToShift.length; i++)
+  {
+
+      var c = textToShift.charAt(i);
+      var res;
+      if(c == c.toLowerCase() && lowercase.indexOf(c)!=-1 ){
+        //alert((lowercase.indexOf(c)+parseInt(shift)));
+        res = lowercase[ (lowercase.indexOf(c)+parseInt(shift))%26 ];
+        
+      }else if( uppercase.indexOf(c)!=-1 ){
+        res = uppercase[ (uppercase.indexOf(c)+parseInt(shift))%26 ];
+      }else{
+        res = c
+      }
+      textShifted +=res;
+  }
+  document.getElementById("caesar_output").value = textShifted;
 }
 
-
-function mapa4(){
-  // Crea un arreglo con los nodos visitados hasta ahora.
+function mapaDelSitio(){
   var nodes = [
       
-      {id: 1, label: 'Brasil'},
+      {id: 1, label: 'Página Web'},
       {id: 2, label: 'Finlandia'},
-      {id: 3, label: 'Japon'},
-      {id: 4, label: 'Rusia'},
-
+      {id: 3, label: 'China'},
   ];
 
   // Crea un arreglo con las conexiones entre los nodos.
   var edges = [
       {from: 1, to: 2},
       {from: 2, to: 3},
-      {from: 3, to: 4},
-  ];
-  // Crea un arreglo con los nodos activos.
-  var activeNode = [ 4 ];
-  mapa( nodes, edges, activeNode );
-}
 
-function nivel5(event) {
-  claveIngresada = document.getElementById("clave").value;
-  claveCorrecta = "1B7an#Se!t.";
-  validacion( claveIngresada,  claveCorrecta );
-}
-
-
-function mapa5(){
-  // Crea un arreglo con los nodos visitados hasta ahora.
-  var nodes = [
-      
-      {id: 1, label: 'Brasil'},
-      {id: 2, label: 'Finlandia'},
-      {id: 3, label: 'Japon'},
-      {id: 4, label: 'Rusia'},
-      {id: 5, label: 'China'},
-  ];
-
-  // Crea un arreglo con las conexiones entre los nodos.
-  var edges = [
-      {from: 1, to: 2},
-      {from: 2, to: 3},
-      {from: 3, to: 4},
-      {from: 4, to: 5},
       
 
   ];
   // Crea un arreglo con los nodos activos.
-  var activeNode = [ 5 ];
-  mapa( nodes, edges, activeNode );
+  var activeNode = [ 3 ];
 }
--- nivel2.js
+++ nivel2.js
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<!--
-	NOTES:
-	1. All tokens are represented by '$' sign in the template.
-	2. You can write your code only wherever mentioned.
-	3. All occurrences of existing tokens will be replaced by their appropriate values.
-	4. Blank lines will be removed automatically.
-	5. Remove unnecessary comments before creating your template.
--->
-<html>
-<head>
-<meta charset="UTF-8">
-<meta name="authoring-tool" content="Adobe_Animate_CC">
-<title>juego</title>
-<!-- write your code here -->
-<script src="https://code.createjs.com/createjs-2015.11.26.min.js"></script>
-<script src="juego.js"></script>
-<script>
 var canvas, stage, exportRoot, anim_container, dom_overlay_container, fnStartAnimation;
 function init() {
 	canvas = document.getElementById("canvas");
@@ -37,7 +19,7 @@ function handleComplete(evt) {
 	for(i=0; i<ssMetadata.length; i++) {
 		ss[ssMetadata[i].name] = new createjs.SpriteSheet( {"images": [queue.getResult(ssMetadata[i].name)], "frames": ssMetadata[i].frames} )
 	}
-	exportRoot = new lib.juego();
+	exportRoot = new lib.juego2();
 	stage = new createjs.Stage(canvas);
 	stage.addChild(exportRoot);
 	stage.enableMouseOver();	
@@ -79,17 +61,6 @@ function handleComplete(evt) {
 			lastW = iw; lastH = ih; lastS = sRatio;		
 		}
 	}
-	makeResponsive(false,'both',false,2);	
+	makeResponsive(false,'both',false,1);	
 	fnStartAnimation();
 }
-</script>
-<!-- write your code here -->
-</head>
-<body onload="init();" style="margin:0px;">
-	<div id="animation_container" style="background-color:rgba(255, 255, 255, 1.00); width:500px; height:354px">
-		<canvas id="canvas" width="500" height="354" style="position: absolute; display: block; background-color:rgba(255, 255, 255, 1.00);"></canvas>
-		<div id="dom_overlay_container" style="pointer-events:none; overflow:hidden; width:500px; height:354px; position: absolute; left: 0px; top: 0px; display: block;">
-		</div>
-	</div>
-</body>
-</html>
